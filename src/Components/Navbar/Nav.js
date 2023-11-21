e4182b734084391a575002d749be4a12fdb19df3
Negar Rezazadeh <negarrezazadeh210@gmail.com>
2023-11-22 02:05:40 +0330

    adding my files to my repository

--- src/Components/Navbar/Nav.js
+++ src/Components/Navbar/Nav.js
@@ -13,7 +13,7 @@ const Navbar = () => {
                 style={({ isActive }) => {
                   return isActive ? { color: "#616DA7" } : {};
                 }}
-                to="/Home"
+                to="/"
               >
                 خانه
               </NavLink>
